9caf91a11f934681f76a6cad634b20a0fe0e04ca
galyastoyanova <kokito_1999@abv.bg>
2018-10-17 10:23:48 +0300

    simple bubble sort

---
+++ GeorgiMalamov_1601261016/Sorting/Sorting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = new[] { 1,20,15,111, 2, 4, 5, 7, 12, 45 };
+
+            Console.WriteLine("Original Array:" + string.Join(",", Array));
+
+            BubbleSort(Array);
+        }
+
+
+        // simple buble sort
+        public static void BubbleSort(int[] Arr)
+        {
+            int i, j;
+            int N = Arr.Length;
+
+            for (j = N - 1; j > 0; j--)
+            {
+                for (i = 0; i < j; i++)
+                {
+                    if (Arr[i] > Arr[i + 1]) {
+                        int temp = Arr[i];
+                        Arr[i] = Arr[i + 1];
+                        Arr[i + 1] = temp;
+                    }
+                }
+            }
+            // print
+            Console.WriteLine("After Sort:" + string.Join(",", Arr));
+        }
+    }
+}
