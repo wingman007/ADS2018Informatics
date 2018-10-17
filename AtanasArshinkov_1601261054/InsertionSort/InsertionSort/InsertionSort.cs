c70cc9921f972fe306ef5e23354d176ab8153e17
Atanas <a.arshinkov97@gmail.com>
2018-10-17 19:38:12 +0300

    Added Insertion Sort

---
+++ AtanasArshinkov_1601261054/InsertionSort/InsertionSort/InsertionSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InsertionSort
+{
+    class InsertionSort
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {-2, 8, 1, 2, 6, -7, 40, 23, 5, 34};
+            Sort(arr);
+            PrintArray(arr);
+        }
+
+        private static void Sort(IList<int> arr)
+        {
+            for (var i = 1; i < arr.Count; ++i)
+            {
+                var key = arr[i];
+                var j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+
+                arr[j + 1] = key;
+            }
+        }
+
+        private static void PrintArray(IReadOnlyList<int> arr)
+        {
+            var n = arr.Count;
+            for (var i = 0; i < n - 1; ++i)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
