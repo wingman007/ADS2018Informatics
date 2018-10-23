6e560c6a51174c058c07795c36301e97aca797c9
martymatbg <martymat@abv.bg>
2018-10-24 02:44:16 +0300

    NumberSystems Test

---
+++ MartinDimitrov_1601261104/NumberSystems/NumberSystems/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberSystems
+{
+    class Program
+    {
+        static string input;
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("Въведете число от десетична бройна система: ");
+                int decimalNumber = int.Parse(Console.ReadLine());
+
+                int remainder;
+                string result = string.Empty;
+                while (decimalNumber > 0)
+                {
+                    remainder = decimalNumber % 2;
+                    decimalNumber /= 2;
+                    result = remainder.ToString() + result;
+                }
+                Console.WriteLine("В двоична бройна система числото е: "+ result);
+            }
+            while (input != "q");
+        }
+    }
+}
