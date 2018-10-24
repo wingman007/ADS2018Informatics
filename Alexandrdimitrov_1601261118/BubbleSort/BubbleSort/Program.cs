3dd7af8271fab290f225e9b4b68f03d9af5d6239
sanika34 <sanyadim@gmail.com>
2018-10-24 17:00:11 +0300

    second commit

---
+++ Alexandrdimitrov_1601261118/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 4, 999, 1, 22, 4, 2, 120, 3213, 35, 23, 24 };
+
+            BubbleSort<int>(arr);
+
+            for (int i = 0; i < arr.Length; i++) {
+                Console.Write(arr[i] + " ");
+            }
+        }
+
+        static void BubbleSort<T>(T[] arr) where T : IComparable {
+            if (arr.Length == 1) {
+                return;
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++) {
+                int j = i + 1;
+                while (j > 0 && arr[j - 1].CompareTo(arr[j]) == 1)
+                {
+                    T temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    j--;
+                }
+            }
+        }
+    }
+}
---
+++ Alexandrdimitrov_1601261118/NumericSystemConversion/README.md
@@ -0,0 +1,3 @@
+
+Numeric System Conversion 
+	- A project for converting from any numeric system to decimal. Can be tweaked to do the opposite conversion.
