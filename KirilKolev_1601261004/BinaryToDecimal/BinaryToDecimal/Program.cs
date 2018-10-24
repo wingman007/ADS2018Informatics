f952af9a69d6d752e32d8803ff9fefed80eea656
MirkoCortez <letq6ia@abv.bg>
2018-10-24 10:07:15 +0300

    Added DecimalToBinary

--- KirilKolev_1601261004/BinaryToDecimal/BinaryToDecimal/Program.cs
+++ KirilKolev_1601261004/BinaryToDecimal/BinaryToDecimal/Program.cs
@@ -8,12 +8,18 @@ namespace Program
     {
         static void Main( string[] args )
         {
+            BinaryToDecimal();
+            DecimalToBinary();
+        }
+
+        static void BinaryToDecimal()
+        {
             int num, binary_val, decimal_val = 0, base_val = 1, rem;
 
             Console.Write( "Enter a Binary Number( 1s and 0s ) : " );
 
-            num = int.Parse( Console.ReadLine() );
-            binary_val = num;
+            num         =   int.Parse( Console.ReadLine() );
+            binary_val  =   num;
 
             while ( num > 0 )
             {
@@ -25,6 +31,26 @@ namespace Program
             Console.Write( "The Binary Number is : " + binary_val );
             Console.Write( "\nIts Decimal Equivalent is : " + decimal_val );
             Console.ReadLine();
+        }
+
+        static void DecimalToBinary()
+        {
+            string answer;
+            string result;
+
+            Console.Write( "Enter a Decimal number : " );
+            answer = Console.ReadLine();
+
+            int num =   Convert.ToInt32( answer );
+            result  =   "";
+            while ( num > 1 )
+            {
+                int remainder   =   num % 2;
+                result          =   Convert.ToString(remainder) + result;
+                num /=  2;
+            }
+            result = Convert.ToString( num ) + result;
+            Console.WriteLine( "Binary: {0}", result );
         }
     }
 }
