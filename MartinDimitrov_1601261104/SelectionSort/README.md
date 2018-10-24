56c98c1f6a610fe33e8fa4e655118e90283101eb
martin-dimitrov-1601261104 <martymat@abv.bg>
2018-10-24 03:33:27 +0300

    Selection Sort Algorithm

---
+++ MartinDimitrov_1601261104/SelectionSort/README.md
@@ -0,0 +1,1 @@
+# Simple Selection sort in C# (console app)
---
+++ MartinDimitrov_1601261104/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 25,29,31,5,14,1001,420,35,46,42 };
+
+            int tmp, min_key;
+
+            for (int j = 0; j < array.Length - 1; j++)
+            {
+                min_key = j;
+
+                for (int k = j + 1; k < array.Length; k++)
+                {
+                    if (array[k] < array[min_key])
+                    {
+                        min_key = k;
+                    }
+                }
+
+                tmp = array[min_key];
+                array[min_key] = array[j];
+                array[j] = tmp;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
