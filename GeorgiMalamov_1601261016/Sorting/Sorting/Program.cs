96a2fe8fae4ded0941ad6870d7823c2a146d2f97
Georgi Malamov <georgimalamov97@gmail.com>
2018-10-27 21:18:26 +0300

    added another sorting method

--- GeorgiMalamov_1601261016/Sorting/Sorting/Program.cs
+++ GeorgiMalamov_1601261016/Sorting/Sorting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Sorting
 {
@@ -6,11 +7,27 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-            int[] Array = new[] { 1,20,15,111, 2, 4, 5, 7, 12, 45 };
+            int[] Array = new[] { 1, 20, 15, 111, 2, 33, 4, 5, 90, 7, 12, 45,1111,2231,13131,144,11,-2,-4,4,777,12 };
 
-            Console.WriteLine("Original Array:" + string.Join(",", Array));
+            Console.WriteLine("Original Array: " + string.Join(",", Array));
 
+
+            // stopwatch for checking the time it takes to sort (or atleast I hope it works!)
+            Stopwatch watch = new Stopwatch();
+            double timePassed;  // time in second, accurate to about millseconds
+
+            watch.Reset();
+            watch.Start();
             BubbleSort(Array);
+            watch.Stop();
+            timePassed = watch.ElapsedMilliseconds / 1000.0; // 1sec == 0,001 ms
+            Console.WriteLine("BubbleSort time: {0:F4}", timePassed);
+
+            watch.Reset();
+            watch.Start();
+            InsertionSort(Array);
+            timePassed = watch.ElapsedMilliseconds / 1000.0; // 1sec == 0,001 ms
+            Console.WriteLine("InsertionSort time: {0:F4}", timePassed);
         }
 
 
@@ -24,7 +41,8 @@ namespace Sorting
             {
                 for (i = 0; i < j; i++)
                 {
-                    if (Arr[i] > Arr[i + 1]) {
+                    if (Arr[i] > Arr[i + 1])
+                    {
                         int temp = Arr[i];
                         Arr[i] = Arr[i + 1];
                         Arr[i + 1] = temp;
@@ -32,7 +50,32 @@ namespace Sorting
                 }
             }
             // print
-            Console.WriteLine("After Sort:" + string.Join(",", Arr));
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("After BubbleSort: " + string.Join(",", Arr));
+            Console.WriteLine("-----------------------------------------------");
+        }
+        
+        // insertion sorting
+        public static void InsertionSort(int[] ArrTemp)
+        {
+            
+            for (int j = 1;  j < ArrTemp.Length; j++)
+            {
+                for (int i = j; i > 0 && ArrTemp[i] < ArrTemp[i - 1]; i--)
+                {
+
+                    int temp = ArrTemp[i];
+                    ArrTemp[i] = ArrTemp[i - 1];
+                    ArrTemp[i - 1] = temp;
+
+                }
+
+            }
+            // print
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("After InsertionSort: " + string.Join(",", ArrTemp));
+            Console.WriteLine("-----------------------------------------------");
+
         }
     }
 }
