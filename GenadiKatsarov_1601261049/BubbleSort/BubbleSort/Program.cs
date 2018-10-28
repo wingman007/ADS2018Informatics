808e965cc3f6beb76d85793aa3d44df40a6713f8
Aynhard <genadi282@abv.bg>
2018-10-28 11:33:07 +0200

    Bubble Sort

---
+++ GenadiKatsarov_1601261049/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 999, 8, 30, 278, 612, 449, 311, 2 };
+
+            int temp = 0;
+
+            for (int write = 0; write < arr.Length; write++)
+            {
+                for (int sort = 0; sort < arr.Length - 1; sort++)
+                {
+                    if (arr[sort] > arr[sort + 1])
+                    {
+                        temp = arr[sort + 1];
+                        arr[sort + 1] = arr[sort];
+                        arr[sort] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+
+            Console.ReadKey();
+        }
+    }
+}
