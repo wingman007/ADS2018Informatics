5e3b72aef107e505840b1f8e8c8e1be9cac4e8e7
I-Ivanov-Ivanov <i-ivanov@abv.bg>
2018-10-29 21:03:47 +0200

    Number System

---
+++ IvanIvanov_1601261013/DecimalToBinary/DecimalToBinary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DecimalToBinary
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string number;
+			bool isNum = false;
+			int num;
+
+			do
+			{
+				Console.WriteLine("Въведете десетично число");
+				number = Console.ReadLine();
+
+				isNum = int.TryParse(number, out num);
+			} while (!isNum);
+
+			int temp = 0;
+			string binary = "";
+
+			do
+			{
+				temp = num % 2;
+				binary = temp + binary;
+				num /= 2;
+			} while (num > 0);
+
+			Console.WriteLine(binary);
+		}
+	}
+}
---
+++ IvanIvanov_1601261013/DecimalToBinary/README.md
@@ -0,0 +1,7 @@
+Numbers Systems part 1:
+Project for converting decimal numbers to binary.
+
+Тo open this program, you need to have a Visual Studio installed;
+Open Visual Studio;
+File -> Open -> Project/Solution -> Select dir -> open file .sln;
+Тo start the project press ctrl+f5;
