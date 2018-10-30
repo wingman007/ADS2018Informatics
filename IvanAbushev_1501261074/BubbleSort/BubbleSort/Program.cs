c4baa852b1bf5b4419b189c51012d1265a3746bb
Ivan Abushev <ivan.abushev0699@abv.bg>
2018-10-30 21:58:07 +0200

    Project Created

---
+++ IvanAbushev_1501261074/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6] { 3, 5, 7, 6, 4, 8 };
+            int temp = 0;
+
+            for (int i = 0;i < arr.Length;i++)
+          {
+                for (int j = 0; j < arr.Length; j++)
+             {
+                    if (arr[i] < arr[j])
+                {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+               }
+           }
+       }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("{0},", arr[i]);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ IvanAbushev_1501261074/NumberSystems/NumberSystems/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NumberSystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//convert a binary to a decimal
+            int m1, m, p = 1;
+            int de = 0, k = 1, j, d;
+            Console.Write("Convert a binary to decimal\n\n");
+            Console.Write("Enter a binary number");
+            m = Convert.ToInt32(Console.ReadLine());
+            m1 = m;
+            for (j = m; j > 0; j = j / 10)
+            {
+                d = j % 10;
+                if (k == 1)
+                    p *= 1;
+                else
+                    p *= 2;
+                de = de + (d * p);
+                k++;
+            }
+            Console.Write("\n The binary number {0} \n The equivalent Decimal number {1}\n\n", m1, de);
+            Console.ReadLine();
+
+           
+        }
+    }
+}
