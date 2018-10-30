8887761c2d95a54340271e9fdb33c4459aeffc0e
Lyubomir Mihaylov <l.mihaylov@abv.bg>
2018-10-30 17:33:53 +0200

    Update convert_to_binary.py

--- LyubomirMihaylov_1601261068/Numeral_Systems/convert_to_binary.py
+++ LyubomirMihaylov_1601261068/Numeral_Systems/convert_to_binary.py
@@ -1,16 +1,24 @@
-
-function convert(decimal):
+def convert(decimal):
     residue = None
-    exact_result = decimal / 2
-    array_result = []
-    while ( exact_result is not 0 ):
+    array_result = [] 
+    # Initially assigned variable for further usage in the loop.
+    exact_result = decimal
+    while ( exact_result >= 0 ):
         residue = exact_result % 2
-        exact_result /= 2
+        exact_result = exact_result // 2
         array_result.append(residue)
+        # When the integer result is 0, save the residue and finish.
+        if exact_result is 0:
+            break
     return array_result
 
 print("Enter decimal:")
 decimal = input()
-string_result = str(decimal)
-final_result = convert(string_result)
-print(final_result)
+integer_input = int(decimal)
+final_result = convert(integer_input)
+
+# Print the binary in reverse order.
+print("Binary result: ")
+for digit in reversed(final_result):
+    # Stay on the same line. ( Append empty string after the last value )
+    print(digit, end='')
