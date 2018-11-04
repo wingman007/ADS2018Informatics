a808745f1d92f988a8ef9f57d13bf008f310bea5
Zhelyazko Georgiev <zhelyazkogg@gmail.com>
2018-11-04 18:04:06 +0200

    Comapring Sorts

---
+++ ZhelyazkoGeorgiev_1601261076/ComparingQuickSorts/ComparingQuickSorts/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingQuickSorts
+{
+    class Program
+    {
+        static void Main(int[] arr, int left, int right)
+        {
+            Console.WriteLine("\n------------ Quick Sort ------------\n");
+
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                if (pivot > 1)
+                {
+                    Main(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                   Main(arr, pivot + 1, right);
+                }
+            }
+
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right]) return right;
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+
+
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6 };
+
+            Console.WriteLine("Original array : ");
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+
+           Main(arr, 0, arr.Length - 1);
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted array : ");
+
+            foreach (var item in arr)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+        }
+    }
+        
+    }
+
