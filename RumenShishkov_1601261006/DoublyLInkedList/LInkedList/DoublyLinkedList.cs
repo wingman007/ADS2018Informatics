b6d64fd8f8f5ea6d553c24e73a37141795930c87
Румен Шишков <nvidia1997@gmail.com>
2018-11-04 14:36:19 +0200

    Pushed Doubly LInked List

---
+++ RumenShishkov_1601261006/DoublyLInkedList/LInkedList/DoublyLinkedList.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+
+namespace LInkedList
+{
+    public class Node
+    {
+        public int data;
+        public Node prevItem, nextItem;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.nextItem = null;
+        }
+        public override string ToString()
+        {
+            return data.ToString();
+        }
+    }
+    public class DoublyLinkedList
+    {
+        private Node headNode;
+        public int Count { get; private set; }
+
+        public bool IsEmpty => headNode == null;
+
+        public DoublyLinkedList()
+        {
+            headNode = null;
+        }
+
+        public Node AddToBeginning(int data)
+        {
+            var tempNode = new Node(data)
+            {
+                nextItem = headNode
+            };
+            if (headNode != null)
+            {
+                headNode.prevItem = tempNode;
+            }
+            headNode = tempNode;
+            Count++;
+            return tempNode;
+        }
+
+        public Node DeleteLast()
+        {
+            Node temp = headNode;
+            if (headNode != null)
+            {
+                headNode = headNode.nextItem;
+                if (headNode != null)
+                {
+                    headNode.prevItem = null;
+                }
+            }
+            Count--;
+            return temp;
+        }
+
+        public override string ToString()
+        {
+            Node currentNode = headNode;
+            StringBuilder builder = new StringBuilder();
+            while (currentNode != null)
+            {
+                builder.Append($"|{currentNode}|");
+                currentNode = currentNode.nextItem;
+            }
+            return builder.ToString();
+        }
+
+    }
+}
---
+++ RumenShishkov_1601261006/DoublyLInkedList/LInkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LInkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myNode = new DoublyLinkedList();
+            myNode.AddToBeginning(7);
+            myNode.AddToBeginning(9);
+            myNode.AddToBeginning(10);
+            myNode.DeleteLast();
+
+            Console.WriteLine($"list empty={myNode.IsEmpty}");
+            Console.WriteLine($"items count is {myNode.Count}");
+            Console.WriteLine("list items:");
+            Console.WriteLine(myNode);
+        }
+    }
+}
