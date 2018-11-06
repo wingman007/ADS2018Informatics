f4732dfd56a1362a2d4478606eb24c48288f5ed2
Baraa <habbalb963@gmail.com>
2018-11-06 21:33:55 +0200

    Implemented the Grouped Queue list.

---
+++ BaraaHabbal_1601261094/GroupsQueueList/GroupsQueueList/QueueList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+//namespace GroupsQueueList
+//{
+//	public class QueueList : public IList<>
+//	{
+//		public QueueList() 
+//		{
+//		}
+//	}
+//}
