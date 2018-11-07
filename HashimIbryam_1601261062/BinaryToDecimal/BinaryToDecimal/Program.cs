4dbc50350b634a7eb58c634851fc93e4ddb87b8e
Xashim <hashim.97@abv.bg>
2018-11-07 11:46:59 +0200

    The first three tasks solved

---
+++ HashimIbryam_1601261062/BinaryToDecimal/BinaryToDecimal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BinaryToDecimal
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a binary number: ");
+            string binaryNumber = Console.ReadLine();
+
+            char[] array = binaryNumber.ToCharArray();
+
+            Array.Reverse(array);
+
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == '1')
+                {
+                    if (i == 0)
+                    {
+                        sum += 1;
+                    }
+                    else
+                    {
+                        sum += (int)Math.Pow(2, i);
+                    }
+                }
+            }
+
+            Console.WriteLine("Result: " + sum);
+        }
+    }
+}
