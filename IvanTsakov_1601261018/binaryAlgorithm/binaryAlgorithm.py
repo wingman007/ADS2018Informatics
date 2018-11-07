b01562019675e7458e9791b4f9adcbe177fe0ee2
Ivan Tsakov <ivantsakov@collegeswimming.com>
2018-11-07 10:02:39 +0200

    Add converting from binary to decimal

--- IvanTsakov_1601261018/binaryAlgorithm/binaryAlgorithm.py
+++ IvanTsakov_1601261018/binaryAlgorithm/binaryAlgorithm.py
@@ -4,8 +4,6 @@ print("Nice to meet you !")
 
 while True:
     num = input("Please insert an integer:")
-
-
     #check if num is int if not return to input.
     if not type(num) == int:
         print("That's not an integer")
@@ -15,3 +13,6 @@ while True:
     # returning the value
     print "The binary number is: " + binary[2:] + 'exiting now....->'
     break
+
+#convert to decimal from binary
+print int("11111111", 2)
