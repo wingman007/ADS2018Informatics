e826e487af80dcc058d1dc31660d729f81731aab
awperincoming <ivanbon13@abv.bg>
2018-11-07 07:36:45 +0200

    Sort

---
+++ IvanBonchev1601261044/Sorting/README.txt
@@ -0,0 +1,4 @@
+This is project for sorting array...
+1.Open Sorting.csproj with Visual Studio 2017
+2.Double click Program.cs on the right in Solution Explorer
+3.Review the code
---
+++ IvanBonchev1601261044/Sorting/Sorting/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number for array size:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double[] arr = new double[n];
+            double b = new double();
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter arr[" + i +"]:");
+                arr[i] = Convert.ToDouble(Console.ReadLine());
+                
+            }
+            for (int i = 1; i < n; i++)
+            {
+                b = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > b)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = b;
+            }
+            Console.WriteLine("The sorted array is:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("arr[" + i + "]:"+arr[i]);
+            }
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
