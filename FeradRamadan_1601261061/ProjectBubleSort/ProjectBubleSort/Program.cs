812e4f047373f3e8a02d8ad04da3dd4afd5b0617
feradrmdn <feradrmdn@gmail.com>
2018-11-07 12:08:27 +0200

    FirstCommit

---
+++ FeradRamadan_1601261061/ProjectBubleSort/ProjectBubleSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectBubleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+                int[] arr = { 97, 38, 58, 76, 69, 24, 27, };
+
+                int temp = 0;
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    for (int j = 0; j < arr.Length - 1 - i; j++)
+                    {
+                        if (arr[j] > arr[j + 1])
+                        {
+                            temp = arr[j + 1];
+                            arr[j + 1] = arr[j];
+                            arr[j] = temp;
+                        }
+                    }
+                }
+
+                for (int i = 0; i < arr.Length; i++) Console.Write(arr[i] + " ");
+
+                Console.ReadKey();
+            
+        }
+    }
+}
+    
+
