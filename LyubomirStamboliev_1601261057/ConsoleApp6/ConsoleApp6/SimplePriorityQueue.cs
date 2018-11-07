a0fedd1a32848c25a65074fcee96536920b43b2d
lyubomir12 <lubomirst09@gmail.com>
2018-11-07 15:11:22 +0200

    ConsoleApp3 and ConsoleApp6

---
+++ LyubomirStamboliev_1601261057/ConsoleApp6/ConsoleApp6/SimplePriorityQueue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Priority_Queue_Example
+{
+    internal class SimplePriorityQueue<T>
+    {
+        internal void Enqueue(string v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void UpdatePriority(string v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
