7e2a6e3a6545b8536f9b75446034e6048638332e
Румен Шишков <nvidia1997@gmail.com>
2018-11-13 23:36:06 +0200

    Implemented Search for elements by key

---
+++ RumenShishkov_1601261006/FindElementsByKey/FindElementsByKey/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindElementsByKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, string>
+            {
+                { "person", "Pesho" },
+                { "make", "Porsche" }
+            };
+
+            FindByKey(dictionary, "make");// search for item with key make
+            FindByKey(dictionary, "person");// search for item with key person
+            FindByKey(dictionary, "animal");// search for item missing item
+        }
+
+        static void FindByKey(Dictionary<string, string> dictionary, string key)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                Console.WriteLine(dictionary[key]);
+                return;
+            }
+            Console.WriteLine($"item with key {key} not found");
+        }
+    }
+}
