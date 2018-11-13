7b2ee4d88c38abc15f17f226df6dccdc077e422f
renetag97 <nachossrg@gmail.com>
2018-11-13 21:37:59 +0200

    Adding Sort, ex.2.47, ex.3.47 and 4.16

---
+++ RenetaGeorgieva_1601261020/CombSort/CombSort/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombSort
+{
+    class Program
+    {
+        public static void CombSort(ref int[] data)
+        {
+            double gap = data.Length;
+            bool swaps = true;
+
+            while (gap > 1 || swaps)
+            {
+                gap /= 1.247330950103979;
+
+                if (gap < 1)
+                    gap = 1;
+
+                int i = 0;
+                swaps = false;
+
+                while (i + gap < data.Length)
+                {
+                    int igap = i + (int)gap;
+
+                    if (data[i] > data[igap])
+                    {
+                        int temp = data[i];
+                        data[i] = data[igap];
+                        data[igap] = temp;
+                        swaps = true;
+                    }
+
+                    ++i;
+                }
+            }
+
+        }
+    }
+}
+        
