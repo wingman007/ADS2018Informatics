9d97a66d1f4cf929e0bc9183b8a71dc804178e50
paveldimchev <dimchev.pavel@gmail.com>
2018-11-14 10:05:52 +0200

    ready

---
+++ LyubomirMetodiev__1601261042/ExpressionRecovory/ExpressionRecovory/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExpressionRecovory
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Моля въведете израз: ");
+            string backwords = Console.ReadLine();
+           
+            int length = backwords.Length;
+            char[] normalString = new char[length];
+
+            for (int i = length-1; i > -1; i--)
+            {
+                normalString[i] = backwords[i];
+            }
+              
+            Console.Write("Нормализиран вид: ");
+            for (int i = length - 1; i > -1; i--)
+            {
+                Console.Write(normalString[i]); 
+            }
+            Console.ReadLine();
+        }
+    }
+}
