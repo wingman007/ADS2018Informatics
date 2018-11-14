bf57b1f84fbe5af6bc56cf5ead723ddc25032cb1
Lyubomir12 <lubomirst09@gmail.com>
2018-11-14 09:46:46 +0200

    ConsoleApp1

---
+++ LyubomirStamboliev_1601261057/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CharSequenceFinder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var array = new[] { 'a', 'b', 'c', 'd', 'e' };
+
+            Console.Write("Въведете последователни букви, които искате да търсите: ");
+            var sequence = Console.ReadLine();
+
+
+            var found = IsExist(array, sequence);
+
+            Console.WriteLine(found);
+        }
+
+        private static bool IsExist(char[] array, string sequence)
+        {
+            var seqArr = sequence.ToCharArray();
+
+            var seqIndex = 0;
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (array[i] == seqArr[seqIndex])
+                {
+                    seqIndex++;
+                    if (seqIndex == seqArr.Length)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    seqIndex = 0;
+                }
+            }
+
+            return false;
+        }
+    }
+}
