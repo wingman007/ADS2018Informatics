e38702ca671a5f9bc1622e4a67ff71c3d1aab794
hristo199 <anubiss_1997@abv.bg>
2018-11-14 13:16:53 +0200

    A small correction

--- HristoBratsov_1601261047/Sorts/PyraSort/PyraSort/Pyra.cpp
+++ HristoBratsov_1601261047/Sorts/PyraSort/PyraSort/Pyra.cpp
@@ -4,22 +4,17 @@ using namespace std;
 void numpat(int n)
 {
 	int num = 26;
-
-
-	for (int i = 0; i < n; i++)
+		for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j <= i; j++)
+			for (int j = 0; j <= i; j++)
 		{
 
-			cout << num << " ";
-
-		
-			num = num - 1;
+				cout << num << " ";
+	num = num - 1;
 		}
-		cout << endl;
+				cout << endl;
 	}
 }
-
 int main()
 {
 	int n = 7;
