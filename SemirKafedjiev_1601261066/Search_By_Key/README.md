0133fe85c230a64912e05b6985ba6d1779b7ac5e
unknown <semir.bv@gmail.com>
2018-11-14 11:51:23 +0200

    Add Search by key

---
+++ SemirKafedjiev_1601261066/Search_By_Key/README.md
@@ -0,0 +1,3 @@
+﻿# To run this application donwoald visual studio or another c# compiler and run the program from Search_By_Key.sln
+За да стартирате програмата трябва да имате инсталиран компилатор за c# самата програма се стартира от Search_By_Key.sln
+
---
+++ SemirKafedjiev_1601261066/Search_By_Key/Search_By_Key/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Search_By_Key
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] names = { "Fury", "Wilder", "Kubrat", "Joshua" };
+            string match = Array.Find(names, delegate (string name)
+            { return name.Contains("K"); });
+            Console.WriteLine(match);
+        }
+    }
+}
+
+   
