3adfdfc44e85033b0f1c4e7314e4f68cba9cda57
paveldimchev <dimchev.pavel@gmail.com>
2018-11-14 10:40:02 +0200

    ready

---
+++ LyubomirMetodiev__1601261042/FindAllElementsWithSameID/FindAllElementsWithSameID/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchByKey
+{
+    public class Program
+    {
+        static IDictionary dictionary = new Dictionary<string, string>();
+
+        public static void Main(string[] args)
+        {
+
+            dictionary.Add("text", "some text");
+            dictionary.Add("jpeg", "some photo");
+            dictionary.Add("mov", "some movie");
+
+            FindEntryValueByKey("jpeg");
+
+            Console.ReadLine();
+        }
+
+        private static void FindEntryValueByKey(string key)
+        {
+            foreach (DictionaryEntry entry in dictionary)
+            {
+                if (entry.Key.Equals(key))
+                {
+                    Console.WriteLine(entry.Value.ToString());
+                    return;
+                }
+            }
+
+            Console.WriteLine("No element with \"{0}\" key was found.", key);
+        }
+    }
+}
