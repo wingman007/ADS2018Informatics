515b071df21b84a8cc54f588fe497e9a9a2e9099
penko-outlov <penko.outlov@gmail.com>
2018-12-03 20:18:08 +0200

    Add files to solve the convex hull problem

---
+++ PenkoOutlov_1601261024/ConvexHull/README.md
@@ -0,0 +1,13 @@
+A program that solves the convex hull problem.
+
+Instructions to run:
+
+- Download and install Python from www.python.org
+- Clone the files on your local machine
+- Use an IDE or text editor to edit the files
+
+For text editor users:
+- Once you have Python 3.5 installed just double click on the file and it will execute.
+
+For IDE users:
+- Run the application from your IDE
---
+++ PenkoOutlov_1601261024/ConvexHull/convexHull.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def split(u, v, points):
+    # return points on left side of UV
+    return [p for p in points if np.cross(p - u, v - u) < 0]
+
+def extend(u, v, points):
+    if not points:
+        return []
+
+    # find furthest point W, and split search to WV, UW
+    w = min(points, key=lambda p: np.cross(p - u, v - u))
+    p1, p2 = split(w, v, points), split(u, w, points)
+    return extend(w, v, p1) + [w] + extend(u, w, p2)
+
+def convex_hull(points):
+    # find two hull points, U, V, and split to left and right search
+    u = min(points, key=lambda p: p[0])
+    v = max(points, key=lambda p: p[0])
+    left, right = split(u, v, points), split(v, u, points)
+
+    # find convex hull on each side
+    return [v] + extend(u, v, left) + [u] + extend(v, u, right) + [v]
